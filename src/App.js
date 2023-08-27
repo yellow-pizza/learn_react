fb8fd597cd70f407c829570bd5ba468ee56c5fd1
fanning <fanning9386@gmail.com>
2023-08-27 22:07:46 +0900

    <> Synax error
    while wrapping multiple adjacent JSX elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+  <>
+    <button className="square">X</button>
+    <button className="square">X</button>
+  </>
+  );
 }
