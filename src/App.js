484de07f20552e3f3b112fef0541d043dbc49d6e
fanning <fanning9386@gmail.com>
2023-09-03 23:47:07 +0900

    put array on Board

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { useState } from 'react';
 function Square({ value }) {
-  function handleClick() {
-    const [value, setValue] = useState(null);
+  const [value, setValue] = useState(null);
 
-    function handleClick() {
-      setValue('X');
+  function handleClick() {
+    setValue('X');
   }
   return (
   <button 
@@ -17,22 +16,23 @@ function Square({ value }) {
     );
 }
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
