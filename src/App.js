1f4acc04c07ca7084aea6521fc3eafcd13768729
fanning <fanning9386@gmail.com>
2023-09-05 23:52:01 +0900

    add function to the Square props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { useState } from 'react';
-function Square({value}) {
-  return <button className="square">{value}</button>;
+function Square({value,onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+  
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
       </div>
