90219d6ce13e9a4d70659ae5df073b6e7895d1c2
fanning <fanning9386@gmail.com>
2023-09-12 23:46:22 +0900

    add state on Game()

--- src/App.js
+++ src/App.js
@@ -80,6 +80,8 @@ function calculateWinner(squares) {
 }
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   return (
     <div className="game">
       <div className="game-board">
