f23457f1e27c87adda3408eeb817e8cf2a6bc2eb
fanning <fanning9386@gmail.com>
2023-09-13 23:54:59 +0900

    currentSquares history

--- src/App.js
+++ src/App.js
@@ -82,6 +82,7 @@ function calculateWinner(squares) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
   return (
     <div className="game">
       <div className="game-board">
