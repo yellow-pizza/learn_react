bc358ef2929866f3d7d5621d97db1849b5ce7ce8
fanning <fanning9386@gmail.com>
2023-09-14 21:13:53 +0900

    add handlePlay

--- src/App.js
+++ src/App.js
@@ -83,10 +83,14 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // TODO
+  }
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
