f4e8ac80830a8c0a37e659419df669331c14e45b
fanning <fanning9386@gmail.com>
2023-09-19 23:56:43 +0900

    Showing the past moves

--- src/App.js
+++ src/App.js
@@ -85,6 +85,24 @@ export default function Game() {
     setXIsNext(!xIsNext);
     // TODO
   }
+
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
   return (
     <div className="game">
       <div className="game-board">
