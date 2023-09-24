0f85280114af88d3c31535033d05d2947b6e9d7e
fanning <fanning9386@gmail.com>
2023-09-24 21:10:18 +0900

    history[currentMove]

--- src/App.js
+++ src/App.js
@@ -79,14 +79,14 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
-    // TODO
+
   }
 
   function jumpTo(nextMove) {
